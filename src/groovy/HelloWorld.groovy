4e283a2e44372871a6efb887f0ccfdbc46ee3976
songzhiyong <songzhiyong1121@gmail.com>
2013-12-06 10:41:13 +0800

    s

---
+++ src/groovy/HelloWorld.groovy
@@ -0,0 +1,8 @@
+package groovy
+
+class HelloWorld {
+	static void main(args) {
+		def var = "song";
+		println "hello world,"+var;
+	}
+}
---
+++ src/groovy/Test.groovy
@@ -0,0 +1,7 @@
+package groovy
+
+class Test {
+	static main(args) {
+		println "hello world";
+	}
+}
